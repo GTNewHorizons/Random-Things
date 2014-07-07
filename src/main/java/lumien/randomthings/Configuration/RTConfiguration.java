b6dd37a49fe3a2a489ab34febd2516bb45a25158
Lumien <lumien231@users.noreply.github.com>
2014-07-07 11:41:54 +0200

    Making certain options require a minecraft restart on change + Removing debug code

--- src/main/java/lumien/randomthings/Configuration/RTConfiguration.java
+++ src/main/java/lumien/randomthings/Configuration/RTConfiguration.java
@@ -19,8 +19,6 @@ public class RTConfiguration
 
 	public static void onConfigChange(OnConfigChangedEvent event)
 	{
-		System.out.println(event.configID);
-
 		if (event.modID.equals(RandomThings.MOD_ID))
 			syncConfig();
 	}
@@ -58,21 +56,21 @@ public class RTConfiguration
 
 		ConfigDungeonLoot.WHITESTONE_CHANCE = RTConfiguration.config.get("DungeonLoot", "WhiteStone", 3).getInt();
 
-		VanillaChanges.MODIFIED_BACKGROUND = RTConfiguration.config.get("VanillaChanges", "ModifiedBackgrounds", true, "The normal dirt background will be replaced with a different block each start").getBoolean(true);
-		VanillaChanges.LOCKED_GAMMA = RTConfiguration.config.get("VanillaChanges", "LockedGamma", false, "Locks the Gamma to 0").getBoolean(false);
+		VanillaChanges.MODIFIED_BACKGROUND = RTConfiguration.config.get("VanillaChanges", "ModifiedBackgrounds", true, "The normal dirt background will be replaced with a different block each start").setRequiresMcRestart(true).getBoolean(true);
+		VanillaChanges.LOCKED_GAMMA = RTConfiguration.config.get("VanillaChanges", "LockedGamma", false, "Locks the Gamma to 0").setRequiresMcRestart(true).getBoolean(false);
 		VanillaChanges.FASTER_LEAVEDECAY = config.get("VanillaChanges", "FasterLeaveDecay", true ,"Leaves will decay much faster when no longer connected to a log").getBoolean(true);
-		VanillaChanges.HARDCORE_DARKNESS = config.get("VanillaChanges", "Hardcore Darkness", false ,"The minimum light will be removed so if there's no light source it's actually going to be completely black").getBoolean(false);
+		VanillaChanges.HARDCORE_DARKNESS = config.get("VanillaChanges", "Hardcore Darkness", false ,"The minimum light will be removed so if there's no light source it's actually going to be completely black").setRequiresMcRestart(true).getBoolean(false);
 		
-		BackgroundHandler.fixedBackground = RTConfiguration.config.get("VanillaChanges", "fixedBackground", "", "If this is not empty the options background will not be random but the one specified here. This has to be the name of a block texture without the .png").getString();
+		BackgroundHandler.fixedBackground = RTConfiguration.config.get("VanillaChanges", "fixedBackground", "", "If this is not empty the options background will not be random but the one specified here. This has to be the name of a block texture without the .png").setRequiresMcRestart(true).getString();
 
 		VanillaChanges.THROWABLES_MOTION = RTConfiguration.config.get("VanillaChanges", "ThrowableMotion", false, "When you throw something or shoot an arrow the motion of the player will be added to the motion of the projectile").getBoolean(false);
 
-		Settings.ANIMATED_TEXTURES = RTConfiguration.config.get("Settings", "AnimatedTextures", true).getBoolean(true);
+		Settings.ANIMATED_TEXTURES = RTConfiguration.config.get("Settings", "AnimatedTextures", true).setRequiresMcRestart(true).getBoolean(true);
 		Settings.FERTILIZEDDIRT_GROWTHINDICATOR = RTConfiguration.config.get("Settings", "FertilizedDirtGrowthIndicator", false, "Bonemeal particles will appear whenever fertilized dirt boosts the plant").getBoolean(false);
 		Settings.SPIRIT_CHANCE = RTConfiguration.config.get("Settings", "SpiritChance", 0.02, "The chance of a spirit spawning when you have a spirit binder in your inventory. (0-1)").getDouble(0.1);
 		Settings.SPIRIT_CHANCE_SWORD = RTConfiguration.config.get("Settings", "SpiritChanceSword", 0.1, "The chance of a spirit spawning when you have a spirit binder in your inventory and kill the entity with a spectre sword. (0-1)").getDouble(0.1);
 		Settings.WIRELESSLEVER_RANGE = RTConfiguration.config.get("Settings", "WirelessLeverRange", 10, "The range of a wireless lever in blocks").getDouble(10);
-		Settings.SPECTRE_DIMENSON_ID = RTConfiguration.config.get("Settings", "SpectreDimensionID", -1, "The Dimension ID of the spectre World. On first run and when you remove this setting from the config file RandomThings will try to find a dimensionID itself").getInt();
+		Settings.SPECTRE_DIMENSON_ID = RTConfiguration.config.get("Settings", "SpectreDimensionID", -1, "The Dimension ID of the spectre World. On first run and when you remove this setting from the config file RandomThings will try to find a dimensionID itself").setRequiresMcRestart(true).getInt();
 
 		MagneticForceHandler.TELEPORT_LENGTH = RTConfiguration.config.get("Settings", "MagneticForceTeleportLength", 200, "In ticks (20=1 Second)").getInt();
 
