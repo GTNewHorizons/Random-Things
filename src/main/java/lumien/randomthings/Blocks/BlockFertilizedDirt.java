877938bb903293a8c523121e040b4b01123e4828
Lumien <lumien231@users.noreply.github.com>
2014-08-09 12:46:30 +0200

    Fixes #20 I should stop testing random stuff in the release code

--- src/main/java/lumien/randomthings/Blocks/BlockFertilizedDirt.java
+++ src/main/java/lumien/randomthings/Blocks/BlockFertilizedDirt.java
@@ -47,14 +47,6 @@ public class BlockFertilizedDirt extends BlockBase
 		this.setBlockTextureName("RandomThings:fertilizedDirt");
 	}
 
-	public void onEntityWalking(World worldObj, int p_149724_2_, int p_149724_3_, int p_149724_4_, Entity entity)
-	{
-		if (!worldObj.isRemote)
-		{
-			entity.setVelocity(0, 20, 0);
-		}
-	}
-
 	@Override
 	public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
 	{
