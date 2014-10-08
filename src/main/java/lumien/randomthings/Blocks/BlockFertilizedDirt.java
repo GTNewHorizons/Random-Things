ccff5161b1cefeac73dfe72a5b4c74fed7d8de52
Lumien <lumien231@users.noreply.github.com>
2014-10-08 11:09:11 +0200

    Fix name of tilled Fertilized Dirt in WAILA

--- src/main/java/lumien/randomthings/Blocks/BlockFertilizedDirt.java
+++ src/main/java/lumien/randomthings/Blocks/BlockFertilizedDirt.java
@@ -46,6 +46,12 @@ public class BlockFertilizedDirt extends BlockBase
 
 		this.setBlockTextureName("RandomThings:fertilizedDirt");
 	}
+	
+	@Override
+	public String getUnlocalizedName()
+	{
+		return "tile.fertilizedDirt";
+	}
 
 	@Override
 	public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
