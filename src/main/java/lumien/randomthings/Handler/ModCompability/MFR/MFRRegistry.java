3a1540e135f0dc1a069ec0c492da80d1bd469cf3
Lumien <lumien231@users.noreply.github.com>
2014-11-01 14:10:15 +0100

    Adding Spirits and other stuff to the Blacklist of Safari Net

---
+++ src/main/java/lumien/randomthings/Handler/ModCompability/MFR/MFRRegistry.java
@@ -0,0 +1,23 @@
+package lumien.randomthings.Handler.ModCompability.MFR;
+
+import java.lang.reflect.Method;
+
+public class MFRRegistry
+{
+	public static void registerSafariNetBlacklist(Class<?> blacklistedEntity)
+	{
+		try
+		{
+			Class<?> registry = Class.forName("powercrystals.minefactoryreloaded.MFRRegistry");
+			if(registry != null)
+			{
+				Method reg = registry.getMethod("registerSafariNetBlacklist", Class.class);
+				reg.invoke(registry, blacklistedEntity);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
