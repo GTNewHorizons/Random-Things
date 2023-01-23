147886dbc634906b98a6e2ab23da74672131b18c
Raven Szewczyk <git@eigenraven.me>
2023-01-23 20:33:27 +0000

    Switch from reflection to access transformer

--- src/main/java/lumien/randomthings/Library/OverrideUtils.java
+++ src/main/java/lumien/randomthings/Library/OverrideUtils.java
@@ -1,19 +1,6 @@
 package lumien.randomthings.Library;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 public class OverrideUtils {
-    public static void setFinalStatic(Field field, Object newValue) throws Exception {
-        field.setAccessible(true);
-
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-
-        field.set(null, newValue);
-    }
-
     public static Class[] convertToClassArray(Object[] objectArray) {
         Class[] classArray = new Class[objectArray.length];
         for (int i = 0; i < objectArray.length; i++) {
